acf00369b2502dd8a3e9ed8460647d228d132388
Jeroen Simonetti <jeroen@simonetti.nl>
2019-05-24 05:43:37 +0200

    Include config
    Signed-off-by: Jeroen Simonetti <jeroen@simonetti.nl>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,10 +10,13 @@ if [[ -n "$UID" ]]; then
     usermod -o -u $UID radicale
 fi
 
+RADICALE_IMAP_HOST="${RADICALE_IMAP_HOST:-lda}"
+
 # Re-set permission to the `radicale` user if current user is root
 # This avoids permission denied if the data volume is mounted by root
 if [ "$1" = 'radicale' -a "$(id -u)" = '0' ]; then
     chown -R radicale:radicale /data
+    sed -i -e 's/<imap_host_variable>/'$RADICALE_IMAP_HOST'/g' /config/config
     exec su-exec radicale "$@"
 fi
 
